d388c58d59ab3715b7d8382fac6dc2d143b1a1f8
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-30 20:48:24 +0800

    override system error msg

--- src/composibles/useSignIn.js
+++ src/composibles/useSignIn.js
@@ -6,17 +6,10 @@ const error = ref(null)
 const signIn = async (email, password) => {
     try {
         const res = await auth.signInWithEmailAndPassword(email, password)
-        if (!res) {
-            throw Error('login failed')
-        }
-        // console.log(res);
-        error.value = null
         console.log(res);
-
-        return res
     } catch (err) {
         console.log(err.message);
-        error.value = err.message
+        error.value = 'incorrect login credential'
     }
 }
 

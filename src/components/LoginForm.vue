1b69e71c292559fcf3d19bf9069c5ac4e0caf23b
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-30 21:01:46 +0800

    emit event in composition api

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -17,17 +17,23 @@
 <script>
 import { ref } from "@vue/reactivity";
 import { useSignIn } from "../composibles/useSignIn";
+import { useRouter } from "vue-router";
 
 export default {
-  setup() {
+  setup(props, context) {
     const email = ref("");
     const password = ref("");
+    const router = useRouter();
 
     const { error, signIn } = useSignIn();
 
     const handleSubmit = async () => {
       console.log(email.value, password.value);
-      await signIn(email.value, password.value);
+      const res = await signIn(email.value, password.value);
+
+      if (!error.value) {
+        context.emit("login");
+      }
     };
 
     return { email, password, handleSubmit, error };
---
+++ src/views/Chatroom.vue
@@ -0,0 +1,10 @@
+<template>
+  <h1>chat room</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>

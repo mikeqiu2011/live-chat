13ae7c4e96ac5f246ad599291f16716c81b6cf4a
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-30 14:14:05 +0800

    first commit

---
+++ src/views/Welcome.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="welcome container">welcome to chat room app</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+</style>

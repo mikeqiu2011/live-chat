3453b56c7fae5c5af2e624a6d6f6a9933757990f
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-31 19:09:39 +0800

    fix problem that user info disapear when refresh

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/main.css'
 
-createApp(App).use(router).mount('#app')
+import './assets/main.css'
+import { auth } from './firebase/config'
+
+let app
+
+auth.onAuthStateChanged(() => { // when page refresh, wait until user login info is fetched
+    if (!app) { // if app already created, no need to recreate
+        app = createApp(App).use(router).mount('#app')
+    }
+
+})
+

0c25d5595e70d8425cac052a4609246ee3bd372e
mikeqiu2011 <mikeqiu2011@163.com>
2022-03-31 20:27:36 +0800

    adding css to chat textarea

--- src/components/NewChatForm.vue
+++ src/components/NewChatForm.vue
@@ -37,5 +37,19 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+form {
+  margin: 10px;
+}
+textarea {
+  width: 100%;
+  max-width: 100%;
+  margin-bottom: 6px;
+  padding: 10px;
+  box-sizing: border-box;
+  border: 0;
+  border-radius: 20px;
+  font-family: inherit;
+  outline: none;
+}
 </style>
